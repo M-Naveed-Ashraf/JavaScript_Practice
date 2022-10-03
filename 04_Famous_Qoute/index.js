20b72c8d69f37e23baabb940ea1efb5205ca577f
M-Naveed-Ashraf <Mahernaveed531@gmail.com>
2022-10-03 11:36:27 +0500

    task 4 completed

---
+++ 04_Famous_Qoute/index.js
@@ -0,0 +1,3 @@
+const quote = "There is none amongst the Muslims who plants a tree or sows seeds, and then a bird, or a person or an animal eats from it, but is regarded as a charitable gift for him."
+
+console.log(`Once the Prophet (ﷺ) said: \"${quote}\"`)
