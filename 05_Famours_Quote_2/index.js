4b8cd4947de07058b3aff5bfaca227bc03efc9b6
M-Naveed-Ashraf <Mahernaveed531@gmail.com>
2022-10-03 11:42:34 +0500

    task 5 completed

---
+++ 05_Famours_Quote_2/index.js
@@ -0,0 +1,4 @@
+const famous_person = 'Prophet (ﷺ)'
+const message = 'There is none amongst the Muslims who plants a tree or sows seeds, and then a bird, or a person or an animal eats from it, but is regarded as a charitable gift for him.';
+
+console.log(`Once the ${famous_person} said, \"${message}\"`);
