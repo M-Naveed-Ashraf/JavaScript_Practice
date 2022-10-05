8000867fd8fd1c8176e207ec6d3627031d7384e4
M-Naveed-Ashraf <Mahernaveed531@gmail.com>
2022-10-05 22:49:08 +0500

    task 20 completed

---
+++ 20_Create_Array/index.js
@@ -0,0 +1,12 @@
+/* create an array of cities
+ we can create an array by two methods */
+
+// using Array constructor
+const cities = Array('Okara', 'Lahore', 'Multan', 'Karachi', 'Islamabad');
+
+console.log(cities);
+
+// by simply assigning the list enclosed in square brackets
+const languages = ['English', 'Urdu', 'Punjabi'];
+
+console.log(languages);
