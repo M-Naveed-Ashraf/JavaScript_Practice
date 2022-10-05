1ac51d5ba2cf1cc8177d6bf780c6da15013e8a07
M-Naveed-Ashraf <Mahernaveed531@gmail.com>
2022-10-05 22:29:52 +0500

    task 18 completed

---
+++ 18_Seeing_the_World/index.js
@@ -0,0 +1,33 @@
+const places = ['London', 'Germany', 'Australia', 'Spain', 'New York'];
+
+// original array
+console.log(places);
+
+// alphabetical sorted array
+const sorted = [...places].sort();
+
+// print alphabetical sorted array
+console.log(sorted);
+
+// print orignal array
+console.log(places);
+
+// print reversed alphabetical sorted array
+console.log(sorted.reverse());
+
+// print orignal array
+console.log(places);
+
+// reverse the original array
+console.log(places.reverse());
+
+// reverse again to get back in original order
+console.log(places.reverse());
+
+// sort original array in alphabetical order
+places.sort();
+console.log(places);
+
+// reverse sorted array
+places.reverse();
+console.log(places);
