edb7ecac5dcc49e009f84d5f190e4ba5625234d2
M-Naveed-Ashraf <Mahernaveed531@gmail.com>
2022-10-06 10:32:16 +0500

    task 23 completed

---
+++ 23_Conditional_Tests/index.js
@@ -0,0 +1,47 @@
+let car = 'subaru';
+
+console.log("Is car == 'subaru'? I predict true.");
+
+console.log(car == 'subaru');
+
+console.log("Is car == 'honda'? I predict false.");
+
+console.log(car == 'honda');
+
+let name = 'Naveed';
+
+console.log("Is typeof name == 'string'? I predict true.");
+
+console.log(typeof name == 'string');
+
+console.log("Is typeof name != 'string'? I predict false.");
+
+console.log(typeof name != 'string');
+
+let num = 5;
+
+console.log("Is typeof num == 'number'? I predict true.");
+
+console.log(typeof num == 'number');
+
+console.log("Is typeof num == 'string'? I predict false.");
+
+console.log(typeof num == 'string');
+
+let arr = [5, 'Hello'];
+
+console.log("arr.includes(5)? I predict true.");
+
+console.log(arr.includes(5));
+
+console.log("arr.includes('5')? I predict false.");
+
+console.log(arr.includes('5'));
+
+console.log("arr.length >= 0? I predict true.");
+
+console.log(arr.length >= 0);
+
+console.log("arr.length < 2? I predict false.");
+
+console.log(arr.length < 2);
