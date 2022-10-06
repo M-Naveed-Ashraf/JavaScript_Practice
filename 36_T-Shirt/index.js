33726e690e4bc15d6ef8baec8d8fd05e7bc6851c
M-Naveed-Ashraf <Mahernaveed531@gmail.com>
2022-10-06 13:35:16 +0500

    task 37 completed

--- 36_T-Shirt/index.js
+++ 36_T-Shirt/index.js
@@ -1,7 +1,5 @@
-function make_shirt(size = 'large', text = 'I love JavaScript') {
+function make_shirt(size, text) {
     console.log(`The size of the shirt is ${size}. message on shirt is \`${text}\'`)
 }
 
-make_shirt();
-make_shirt('medium');
-make_shirt('small', 'Don\'t Give Up');
+make_shirt('large', 'Don\'t Give Up');
---
+++ 37_Large_Shirts/index.js
@@ -0,0 +1,7 @@
+function make_shirt(size = 'large', text = 'I love JavaScript') {
+    console.log(`The size of the shirt is ${size}. message on shirt is \`${text}\'`)
+}
+
+make_shirt();
+make_shirt('medium');
+make_shirt('small', 'Don\'t Give Up');
