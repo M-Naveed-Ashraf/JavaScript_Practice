187bfeefd2d62a836135f646c72620c99ec4d5e2
M-Naveed-Ashraf <Mahernaveed531@gmail.com>
2022-10-06 12:27:17 +0500

    task 32 completed

---
+++ 32_Checking_Usernames/index.js
@@ -0,0 +1,10 @@
+let current_users = ['Admin', 'Naveed', 'Ali', 'Eric', 'Siko'];
+let new_users = ['Asad', 'Naveed', 'Ahmad', 'Zia', 'Ali'];
+
+new_users.forEach( elem => {
+    if(current_users.includes(elem)){
+        console.log(`${elem} will need to enter a new username.`);
+    } else {
+        console.log(`${elem} is available.`);
+    }
+})
