edbcfe60fddb1a05335b802519c061eb1b66e6a0
M-Naveed-Ashraf <Mahernaveed531@gmail.com>
2022-10-06 14:24:43 +0500

    task 41 completed

---
+++ 41_Megicians/index.js
@@ -0,0 +1,9 @@
+const megicians = ['megician 1', 'megician 2', 'megician 3'];
+
+function show_magicians(megicians) {
+    megicians.forEach( elem => {
+        console.log(elem);
+    }) 
+}
+
+show_magicians(megicians)
