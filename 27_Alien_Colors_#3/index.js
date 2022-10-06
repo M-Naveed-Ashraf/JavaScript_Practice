8db378cf36c804063e85b243ce2681c4d12d1b1c
M-Naveed-Ashraf <Mahernaveed531@gmail.com>
2022-10-06 11:38:21 +0500

    task 27 completed

---
+++ 27_Alien_Colors_#3/index.js
@@ -0,0 +1,33 @@
+
+//      Version #1
+let alien_color = 'green';
+
+if(alien_color === 'green'){
+    console.log('v1: The player just earned 5 points.');
+} else if (alien_color === 'yellow'){
+    console.log('v1: The player just earned 10 points.');
+} else {
+    console.log('v1: The player just earned 15 points.');
+}
+
+//      Version #2
+let alien_color_v2 = 'yellow';
+
+if(alien_color_v2 === 'green'){
+    console.log('v2: The player just earned 5 points.');
+} else if (alien_color_v2 === 'yellow'){
+    console.log('v2: The player just earned 10 points.');
+} else {
+    console.log('v2: The player just earned 15 points.');
+}
+
+//      Version #3
+let alien_color_v3 = 'red';
+
+if(alien_color_v3 === 'green'){
+    console.log('v3: The player just earned 5 points.');
+} else if (alien_color_v3 === 'yellow'){
+    console.log('v3: The player just earned 10 points.');
+} else {
+    console.log('v3: The player just earned 15 points.');
+}
