b9b854c36d292d1052a73bc9bd989daf7427eb37
M-Naveed-Ashraf <Mahernaveed531@gmail.com>
2022-10-06 12:01:51 +0500

    task 30 completed

---
+++ 30_Hello_Admin/index.js
@@ -0,0 +1,9 @@
+const userNames = ['Admin', 'Naveed', 'Ali', 'Eric', 'Siko'];
+
+userNames.forEach( elem => {
+    if(elem === 'Admin'){
+        console.log('Hello admin, would you like to see a status report?');
+    } else {
+        console.log(`Hello ${elem}, thank you for logging in again.`)
+    }
+})
