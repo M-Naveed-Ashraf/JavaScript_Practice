e60509d0aa58a9692cd279a2dca03903e28d487d
M-Naveed-Ashraf <Mahernaveed531@gmail.com>
2022-10-06 13:39:02 +0500

    task 38 completed

---
+++ 38_Cities/index.js
@@ -0,0 +1,7 @@
+function describe_city(name, country='Pakistan') {
+    console.log(`${name} is in ${country}.`)
+}
+
+describe_city('Lahore');
+describe_city('Karachi', 'Pakistan');
+describe_city('Delhi', 'India');
