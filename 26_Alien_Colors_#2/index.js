dbd8b8494bfaf67753b450b5959dc53545de291d
M-Naveed-Ashraf <Mahernaveed531@gmail.com>
2022-10-06 11:33:36 +0500

    task 26 completed

---
+++ 26_Alien_Colors_#2/index.js
@@ -0,0 +1,19 @@
+
+//      Version #1
+let alien_color = 'green';
+
+if(alien_color === 'green'){
+    console.log('v1: The player just earned 5 points.');
+} else {
+    console.log('v1: The player just earned 10 points.');
+}
+
+
+//      Version #2
+let alien_color_v2 = 'yellow';
+
+if(alien_color_v2 === 'green'){
+    console.log('v2: The player just earned 5 points.');
+} else {
+    console.log('v2: The player just earned 10 points.');
+}
