4ea0afd0dfb8ac8213d16ce160a7443940e27bba
M-Naveed-Ashraf <Mahernaveed531@gmail.com>
2022-10-06 11:55:04 +0500

    task 29 completed

---
+++ 29_Favourite_Fruit/index.js
@@ -0,0 +1,17 @@
+let favorite_fruits = ['Mango', 'Grapes', 'Apple'];
+
+if(favorite_fruits.includes('pineapple')){
+    console.log('such as You really like pineapple!');
+}
+if(favorite_fruits.includes('Mango')){
+    console.log('such as You really like Mango!');
+}
+if(favorite_fruits.includes('Grapes')){
+    console.log('such as You really like Grapes!');
+}
+if(favorite_fruits.includes('Apple')){
+    console.log('such as You really like Apple!');
+}
+if(favorite_fruits.includes('Orange')){
+    console.log('such as You really like Orange!');
+}
