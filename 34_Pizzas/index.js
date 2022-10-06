3096106c881ee53516b6ce57421ae68074ddc163
M-Naveed-Ashraf <Mahernaveed531@gmail.com>
2022-10-06 13:12:46 +0500

    task 34 completed

---
+++ 34_Pizzas/index.js
@@ -0,0 +1,14 @@
+const piza_flavour = ['Makhani Cheese Pizza', 'Saag Paneer Pizza', 'Malai Paneer Pizza'];
+
+for(let i of piza_flavour) {
+    console.log(`I like ${i}`);
+}
+
+console.log(
+    `
+    Humans are drawn to foods that are fatty, sweet, rich and complex. 
+    Pizza has all of these components. Cheese is fatty, meat toppings tend to be rich and the sauce is sweet. 
+    Pizza toppings are also packed with a compound called glutamate, which can be found in the tomatoes, cheese, pepperoni and sausage.
+    I really love pizza!
+    `
+)
